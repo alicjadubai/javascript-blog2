3a16a90522595e13f2250ef389576a71a6c4d33e
Alicja Jagielska <alicja_dubai@wp.pl>
2021-12-30 11:07:52 +0100

    GenerateTags

--- js/script.js
+++ js/script.js
@@ -32,7 +32,8 @@ function titleClickHandler(event) {
 
 const optArticleSelector = ".post",
   optTitleSelector = ".post-title",
-  optTitleListSelector = ".titles";
+  optTitleListSelector = ".titles",
+  optArticleTagsSelector = ".post-tags .list";
 
 function generateTitleLinks() {
   /* remove contents of titleList */
@@ -69,3 +70,39 @@ const links = document.querySelectorAll(".titles a");
 for (let link of links) {
   link.addEventListener("click", titleClickHandler);
 }
+
+function generateTags() {
+  /* find all articles */
+  const articles = document.querySelectorAll(optArticleSelector);
+  console.log("tags articles", articles);
+  /* START LOOP: for every article: */
+  for (let article of articles) {
+    /* find tags wrapper */
+    const tagsWrapper = article.querySelector(optArticleTagsSelector);
+    console.log(tagsWrapper);
+    /* make html variable with empty string */
+    let html = "";
+    /* get tags from data-tags attribute */
+    const articleTags = article.getAttribute("data-tags");
+    console.log(articleTags);
+    /* split tags into array */
+    const articleTagsArray = articleTags.split(" ");
+    console.log(articleTagsArray);
+    /* START LOOP: for each tag */
+    for (let tag of articleTagsArray) {
+      /* generate HTML of the link */
+      const linkHTML = `<li><a href="#tag-${tag}">${tag}</a></li>`;
+      console.log(linkHTML);
+      /* add generated code to html variable */
+      html = html + linkHTML;
+      console.log(html);
+      /* END LOOP: for each tag */
+    }
+    /* insert HTML of all the links into the tags wrapper */
+    tagsWrapper.innerHTML = html;
+    console.log(tagsWrapper);
+    /* END LOOP: for every article: */
+  }
+}
+
+generateTags();
